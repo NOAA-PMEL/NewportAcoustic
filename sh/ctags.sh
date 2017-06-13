8e6bfb1be8f33d2e825aed653e74ceab7f960ceb
Brian Kahn <brian.kahn@noaa.gov>
2017-06-13 16:36:54 -0700

    ctags #include.h, doc/parameters

--- sh/ctags.sh
+++ sh/ctags.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # make lara tags file
-cd ~/NewportAcoustic
-ctags -L sh/filelist.lara -o .tags
+if [ $# -eq 0 ]; then
+  Files="$(cat ~/NewportAcoustic/sh/filelist.lara)"
+else
+  Files="$*"
+fi
+Headers="$(find ~/cf2/headers/*/Headers -name '*.h')"
+
+ctags -o .tags $Files $Headers
+
+
+
+#cd ~/NewportAcoustic
+#ctags -L sh/filelist.lara -o .tags
 
 
 #files="MPC/ADS/ADS.c LARA/LARA.c MPC/CTD/CTD.c MPC/GPSIRID/GPSIRID.c MPC/WISPR/WISPR.c MPC/WINCH/Winch.c MPC/MPC_SETTINGS/Settings.c MPC/MPC_Global/MPC.c MPC/CTD/CTD.h MPC/GPSIRID/GPSIRID.h MPC/WISPR/WISPR.h MPC/WINCH/Winch.h MPC/MPC_SETTINGS/Settings.h LARA/PLATFORM.h MPC/MPC_Global/MPC_Global.h MPC/ADS/ADS.h"
