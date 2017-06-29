c27b22e44611a1164a05a4ae38aae92c514f62f2
Brian Kahn <brian.kahn@noaa.gov>
2017-06-29 12:15:35 -0700

    bugfix: stuck in dock. improved info(), emul help

--- bench/emul/emul.py
+++ bench/emul/emul.py
@@ -4,55 +4,66 @@ from shared import *
 
 
 # program parameters
-buoyArgs = {}
-winchArgs = {}
-radioArgs = {}
+#buoyArgs = {}
+#winchArgs = {}
+#radioArgs = {}
+#
+#def init():
+#    "set initial values"
+#    global winchArgs, buoyArgs, radioArgs
+#    # arguments
+#    for a in sys.argv[1:]:
+#        f = a.find('=')+1
+#        arg = a[f:]
+#        if '-?' in a:
+#            print "usage: -syncmode=y -cable=# -mooring=#" + \
+#                " -amodDelay=# -?"
+#        if '-s' in a: 
+#            buoyArgs['syncmode'] = 1
+#            print "syncmode on"
+#        elif '-c' in a:
+#            winchArgs['cableLen']=float(arg)
+#            print "cable %s" % arg
+#        elif '-m' in a:
+#            winchArgs['mooring']=int(arg)
+#            print "mooring %s" % arg
+#        elif '-a' in a:
+#            winchArgs['amodDelay'] = float(arg)
+#            print "amodem delay %s" % arg
+#
+#    # initialize objects
+#    if buoyArgs: buoy.modGlobals(**buoyArgs)
+#    if winchArgs: winch.modGlobals(**winchArgs)
+#    if radioArgs: radio.modGlobals(**winchArgs)
 
 def init():
-    "set initial values"
-    global winchArgs, buoyArgs, radioArgs
-    # arguments
-    for a in sys.argv[1:]:
-        f = a.find('=')+1
-        arg = a[f:]
-        if '-?' in a:
-            print "usage: -syncmode=y -cable=# -mooring=#" + \
-                " -amodDelay=# -?"
-        if '-s' in a: 
-            buoyArgs['syncmode'] = 1
-            print "syncmode on"
-        elif '-c' in a:
-            winchArgs['cableLen']=float(arg)
-            print "cable %s" % arg
-        elif '-m' in a:
-            winchArgs['mooring']=int(arg)
-            print "mooring %s" % arg
-        elif '-a' in a:
-            winchArgs['amodDelay'] = float(arg)
-            print "amodem delay %s" % arg
-
-    # initialize objects
-    if buoyArgs: buoy.modGlobals(**buoyArgs)
-    if winchArgs: winch.modGlobals(**winchArgs)
-    if radioArgs: radio.modGlobals(**winchArgs)
+    "init all"
+    buoy.init()
+    winch.init()
+    # radio.init()
 
 def start():
-    "start up"
+    "start all"
     buoy.start()
-    radio.start()
     winch.start()
+    # radio.start()
 
 def stop():
-    "close down"
+    "stop all"
     buoy.stop()
-    radio.stop()
     winch.stop()
+    # radio.stop()
 
+def info():
+    "info all"
+    buoy.info()
+    winch.info()
+    # radio.info()
+    
 
-
-if __name__=='__main__': init(); start()
-else: print "init(); start(); stop()   or buoy.start(); winch.start()"
+if __name__=='__main__': start()
+else: print "start() stop() init() buoy.info() winch.start() winch.cable=15"
 
 
 # Notes:
-# add exception handling for errs
+# add exception ^C stop() before exit
